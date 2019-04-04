16c91b5367055156dc7e1c94d789170c6b9edb67
John Barrett <johnjbarrett22@gmail.com>
2019-04-03 18:21:46 -0700

    Established the majority of the validations, though will need to come back to complete and finish encryption

--- apps/message_board/models.py
+++ apps/message_board/models.py
@@ -13,7 +13,7 @@ class User(models.Model):
 class Message(models.Model):
     message = models.TextField()
     messager = models.ForeignKey(
-        User, related_name="messages_made", on_delete=models.CASCADE)
+        User, related_name="messages_made_by", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -21,7 +21,7 @@ class Message(models.Model):
 class Comment(models.Model):
     comment = models.TextField()
     commentor = models.ForeignKey(
-        User, related_name="comments_made", on_delete=models.CASCADE)
+        User, related_name="comments_made_by", on_delete=models.CASCADE)
     post = models.ForeignKey(
         Message, related_name="post_comments", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
--- apps/message_board/views.py
+++ apps/message_board/views.py
@@ -1,5 +1,51 @@
 from django.shortcuts import render, redirect, HttpResponse
+from django.contrib import messages
+from .models import *
+import bcrypt
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 
 def index(request):
-    return HttpResponse("Hello World!")
+    return render(request, "message_board/index.html")
+
+
+def register(request):
+    error = False
+    if len(request.POST['first_name']) < 2:
+        error = True
+        messages.error(
+            request, "First name must be a minimum length of three characters!")
+
+    if request.POST(['first_name']).isalpha() == False:
+        error = True
+        messages.error(
+            request, "First name cannot have a number in it or be blank!")
+
+    if len(request.POST['last_name']) < 2:
+        error = True
+        messages.error(
+            request, "Last name must be a minimum of three characters!")
+
+    if request.POST(['last_name']).isalpha() == False:
+        error = True
+        messages.error(
+            request, "Last name cannot have a number in it or be blank!")
+
+    if len(request.POST['email']) < 7:
+        error = True
+        messages.error(
+            request, "Email must be a minimum length of seven characters!")
+
+    if not EMAIL_REGEX.match(request.post['email']):
+        error = True
+        messages.error(
+            request, "Email must be a valid email address!")
+
+    if len(request.POST['password']) < 8:
+        error = True
+        messages.error(
+            request, "Password must be a minimum length of eight characters!")
+
+    if request.POST['password'] != request.POST['confirm_password']:
+        error = True
